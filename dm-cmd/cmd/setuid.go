6b50f9e5fecb226dd9af41d79525bc2cdc9fddd8
Young Yu <young@Youngs-MacBook-Pro-2.local>
2024-02-16 15:15:46 -0800

    1) signed dm-cmd_darwin_amd64, dm-cmd_darwin_arm64 2) added setuid for mac/linux

---
+++ dm-cmd/cmd/setuid.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"io/fs"
+//	"syscall"
+
+	"github.com/kardianos/osext"
+	"github.com/spf13/cobra"
+//	"golang.org/x/sys/unix"
+)
+
+func init() {
+	rootCmd.AddCommand(setUidCmd)
+}
+
+var setUidCmd = &cobra.Command{
+	Use: "setuid",
+	Run: func(cmd *cobra.Command, args []string) {
+		err := SetUid()
+		if err != nil {
+			fmt.Println("setuid failed %v.", err.Error())
+		}
+	},
+}
+
+// set self binary as root/wheel and execute bits
+func SetUid() error {
+	self, err := osext.Executable()
+	if err != nil {
+		return err
+	}
+
+	err = os.Chown(self, 0, 0)
+	if err != nil {
+		log.Printf("change binary to root failed, binary %v, error %v", self, err)
+		return err
+	}
+
+//	err = syscall.Chmod(self, syscall.S_IWUSR|syscall.S_IRWXU|unix.S_IRGRP|syscall.S_IXGRP|syscall.S_IROTH|syscall.S_IXOTH|syscall.S_ISUID)
+	err = os.Chmod(self, 0755|fs.ModeSetuid)
+	if err != nil {
+		log.Printf("change binary to root failed, binary %v error %v", self, err)
+		return err
+	}
+
+	log.Printf("set binary %v successfully", self)
+	return nil
+}
--- dm-cmd/go.mod
+++ dm-cmd/go.mod
@@ -1,9 +1,13 @@
 module github.com/kitecyber/dm/dm-cmd
 
-go 1.21.0
+go 1.21.6
+
+require (
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0
+	github.com/spf13/cobra v1.8.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
